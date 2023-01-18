18fac6e55d26351ed597e246aedc527291871bf1
serdargoleli <web.serdargoleli@gmail.com>
2023-01-19 01:19:22 +0300

    SEO updated

--- pages/_app.js
+++ pages/_app.js
@@ -3,10 +3,16 @@ import Layout from "@/layouts/Layout";
 import store from "../store/index";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "styles/index.css";
+import Head from "next/head";
 
 const App = ({ Component, pageProps }) => {
   return (
     <Provider store={store}>
+      <Head>
+        <title>S'GAME FREE GAME PLAY TO</title>
+        <meta name="author" content="Serdar Göleli" />
+        <meta name="description" content="Free games to play on PC and Browsers" />
+      </Head>
       <Layout>
         <Component {...pageProps} />
       </Layout>
